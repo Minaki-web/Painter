a15cbe28549748845f91e7186c03cb0f9e173470
Minaki-web <mine.mnk0627@gmail.com>
2021-03-13 14:48:30 +0800

    Revert "add link at footer"
    This reverts commit 019232a8f27468f6ff0d32f1e137f5fb47324b10.

--- js/draw.js
+++ js/draw.js
@@ -1,1 +1,32 @@
-const canvas = document.getElementById('drawingArea');
+window.onload = function() {
+  const canvas = document.getElementById("drawingArea");
+  const ctx = canvas.getContext("2d");
+
+  // ここからリサイズ処理
+  canvas.width = 1000;
+  function fitCanvas() {
+    // margin: 0 10px
+    canvas.width = document.documentElement.clientWidth - 20;
+
+    // 1000px以上にしない
+    if(canvas.width > 1000){
+      canvas.width = 1000;
+    }
+  }
+  window.onresize = fitCanvas;
+  // ここまでリサイズ処理
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
